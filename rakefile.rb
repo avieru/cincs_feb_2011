67c5d265ea09d4ee3a4c7a5544c185d3bdaa9406
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2011-01-23 21:18:32 -0500

    Added a bunch of new files

--- rakefile.rb
+++ rakefile.rb
@@ -1,6 +1,7 @@
 require 'rake'
 require 'rake/clean'
 require 'fileutils'
+require 'build/project.rb'
 
 ['build/tools/Rake','build'].each do|pattern|
   Dir.glob(File.join(File.dirname(__FILE__),pattern,"*.rb")).each do|item|
@@ -10,7 +11,8 @@ require 'fileutils'
 end
 
 #load settings that differ by machine
-local_settings = LocalSettings.new
+database_details = DbDetails.new
+local_settings = LocalSettings.new(database_details)
 
 COMPILE_TARGET = 'debug'
 
@@ -26,15 +28,21 @@ template_code_dir = File.join('product','templated_code')
 
 #configuration files
 config_files = FileList.new(File.join('product','config','*.template')).select{|fn| ! fn.include?('app.config')}
+
 app_config = TemplateFile.new(File.join('product','config',local_settings[:app_config_template]))
 
+def create_sql_fileset(folder)
+  FileList.new(File.join('product','sql',folder,'**/*.sql'))
+end
+
+sql_runner = SqlRunner.new(database_details)
+#
 #target folders that can be run from VS
 solution_file = "solution.sln"
 
-task :default => ["specs:run"]
+task :default => ["db:create_schema","specs:run"]
 
 task :init  => :clean do
-  cp_r "thirdparty/machine.specifications/.","artifacts"
   [Project.specs_dir,Project.report_folder].each{|folder| mkdir folder if ! File.exists?(folder)}
 end
 
@@ -43,15 +51,6 @@ task :expand_all_template_files do
   template_files.generate_all_output_files(local_settings.settings)
 end
 
-desc 'builds the database'
-task :build_db => :expand_all_template_files do
-    sql_runner.process_sql_files(create_sql_fileset('ddl'))
-end
-
-desc 'load the database'
-task :load_data => :build_db do
-    sql_runner.process_sql_files(create_sql_fileset('data'))
-end
 
 desc 'compiles the project'
 task :compile => :expand_all_template_files do
@@ -66,6 +65,20 @@ task :from_ide  do
 
   config_files.each do |file|
     TemplateFile.new(file).generate_to_directories([Project.startup_dir,'artifacts'],local_settings.settings)
+  end
+end
+
+namespace :db do
+  desc 'tears down the database and recreates it from the ddl files'
+  task :create_schema => :expand_all_template_files do
+      sql_runner.process_sql_files(create_sql_fileset('ddl'))
+
+      sh "build/tools/sql_metal/SqlMetal.exe /server:#{database_details.server_name} /database:#{database_details.initial_catalog} /code:product/nothinbutdotnetstore/dataaccesslayer/generated/ReportingModels.cs /namespace:nothinbutdotnetstore.dataaccesslayer.generated"
+  end
+
+  desc 'loads the database with acceptance testing data'
+  task :load_data => :create_schema do
+      sql_runner.process_sql_files(create_sql_fileset('data'))
   end
 end
 
