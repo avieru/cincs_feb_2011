19499ddf3b4ca377ca3a80bc467a72494ce000e8
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2011-02-08 20:52:01 -0500

    Updated the spec

--- product/nothinbutdotnetstore.specs/CommandRegistrySpecs.cs
+++ product/nothinbutdotnetstore.specs/CommandRegistrySpecs.cs
@@ -12,10 +12,41 @@ namespace nothinbutdotnetstore.specs
         public abstract class concern : Observes<CommandRegistry,
                                             DefaultCommandRegistry>
         {
+            Establish c = () =>
+            {
+                request = an<Request>();
+                all_commands = Enumerable.Range(1, 100).Select(x => an<RequestCommand>()).ToList();
+
+                provide_a_basic_sut_constructor_argument<IEnumerable<RequestCommand>>(all_commands);
+            };
+
+            protected static RequestCommand result;
+            protected static Request request;
+            protected static IList<RequestCommand> all_commands;
         }
 
         [Subject(typeof(DefaultCommandRegistry))]
         public class when_finding_a_command_that_can_process_a_request_and_it_has_the_command : concern
+        {
+            Establish c = () =>
+            {
+                the_command_that_can_handle_the_request = an<RequestCommand>();
+                all_commands.Add(the_command_that_can_handle_the_request);
+                the_command_that_can_handle_the_request.Stub(x => x.can_handle(request))
+                    .Return(true);
+            };
+
+            Because b = () =>
+                result = sut.get_the_command_that_can_process(request);
+
+            It should_return_the_command_that_can_process_the_request = () =>
+                result.ShouldEqual(the_command_that_can_handle_the_request);
+
+            static RequestCommand the_command_that_can_handle_the_request;
+        }
+
+        [Subject(typeof(DefaultCommandRegistry))]
+        public class when_attempting_to_find_a_command_that_can_process_a_request_and_it_does_not_have_the_command : concern
         {
             Establish c = () =>
             {
