d143b6d8618983f3a30feb1827a84dd6d6b0bd09
Andrei <avieru@gmail.com>
2011-02-08 20:51:12 -0500

    Pushing new changes

--- product/nothinbutdotnetstore/web/core/DefaultCommandRegistry.cs
+++ product/nothinbutdotnetstore/web/core/DefaultCommandRegistry.cs
@@ -1,12 +1,21 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace nothinbutdotnetstore.web.core
 {
     public class DefaultCommandRegistry : CommandRegistry
     {
+        readonly IEnumerable<RequestCommand> all_commands;
+
+        public DefaultCommandRegistry(IEnumerable<RequestCommand> all_commands)
+        {
+            this.all_commands = all_commands;
+        }
+
         public RequestCommand get_the_command_that_can_process(Request request)
         {
-            throw new NotImplementedException();
+            return all_commands.ToList().Find(x => x.can_handle(request));
         }
     }
 }
