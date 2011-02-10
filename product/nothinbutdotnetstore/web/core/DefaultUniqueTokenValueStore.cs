64fb12b12643ba494d399e16c06e5d7faab6c139
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2011-02-10 14:50:05 -0500

    Working on finishing url builder

--- product/nothinbutdotnetstore/web/core/DefaultUniqueTokenValueStore.cs
+++ product/nothinbutdotnetstore/web/core/DefaultUniqueTokenValueStore.cs
@@ -1,20 +1,29 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace nothinbutdotnetstore.web.core
 {
     public class DefaultUniqueTokenValueStore : UniqueTokenValueStore
     {
-        
+        public IDictionary<string, object> tokens = new Dictionary<string, object>();
+
         public IEnumerator<KeyValuePair<string, object>> GetEnumerator()
         {
-            throw new NotImplementedException();
+            return tokens.Select(pair => new KeyValuePair<string, object>(pair.Key, pair.Value)).GetEnumerator();
         }
 
         public void store_token_value(string token_key, object value)
         {
-            throw new NotImplementedException();
+            if (token_exists_for(token_key)) return;
+
+            tokens.Add(token_key, value);
+        }
+
+        bool token_exists_for(string token_key)
+        {
+            return tokens.ContainsKey(token_key);
         }
 
         public bool is_already_in_the_store(string token_key)
