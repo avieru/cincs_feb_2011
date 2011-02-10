6dd9429a74144b0247c0260a51f74b1738117229
Andrei <avieru@gmail.com>
2011-02-10 09:58:24 -0500

    Pushing new changes

--- product/nothinbutdotnetstore/web/core/UrlEncodingKeyValuePairVisitor.cs
+++ product/nothinbutdotnetstore/web/core/UrlEncodingKeyValuePairVisitor.cs
@@ -1,13 +1,21 @@
 using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace nothinbutdotnetstore.web.core
 {
     public class UrlEncodingKeyValuePairVisitor
     {
+        StringBuilder string_builder;
+
+        public UrlEncodingKeyValuePairVisitor(StringBuilder string_builder)
+        {
+            this.string_builder = string_builder;
+        }
+
         public void process(KeyValuePair<string, object> first_token)
         {
-            throw new NotImplementedException();
+            string_builder.Append(string.Format("{0}.cinc", first_token.Value));
         }
     }
 }
