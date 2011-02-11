75eeb0f123be69dcb55beab6be9f1b101e68e1e5
Andrei <avieru@gmail.com>
2011-02-11 13:32:02 -0500

    Pushing new changes

--- product/nothinbutdotnetstore/tasks/startup/StartUp.cs
+++ product/nothinbutdotnetstore/tasks/startup/StartUp.cs
@@ -15,35 +15,35 @@ namespace nothinbutdotnetstore.tasks.startup
     {
         public static void run()
         {
-            var command_registry = new DefaultCommandRegistry(new StubSetOfCommands());
-            var front_controller = new DefaultFrontController(command_registry);
-
             DependencyContainer container = new BasicDependencyContainer(
-                new BasicDependencyFactories(generate_factories(front_controller), WebFactories.special_case_dependency_factory));
+                new BasicDependencyFactories(get_all_setup_factories(), WebFactories.special_case_dependency_factory));
 
             Container.container_factory = () => container;
         }
 
-
-        static WebFormViewFactory get_webform_factory()
+        static IDictionary<Type, DependencyFactory> get_all_setup_factories()
         {
-            var form_path_registry = new StubFormPathRegistry();
-            return new WebFormViewFactory(form_path_registry, BuildManager.CreateInstanceFromVirtualPath);
-        }
-
-
-        static IDictionary<Type, DependencyFactory> generate_factories(FrontController front_controller)
-        {
+            var front_controller = new DefaultFrontController(new DefaultCommandRegistry(new StubSetOfCommands()));
             var catalog = new StubCatalog();
             var renderer = new WebFormRenderer(() => HttpContext.Current, get_webform_factory());
+
             IDictionary<Type, DependencyFactory> all_factories = new Dictionary<Type, DependencyFactory>();
-            all_factories.Add(typeof(FrontController), with_exception_handling<DefaultFrontController>(() => front_controller));
-            all_factories.Add(typeof(RequestFactory),with_exception_handling<RequestFactory>(() => new StubRequestFactory()));
-            all_factories.Add(typeof(Renderer),with_exception_handling<RequestFactory>(() => renderer));
-            all_factories.Add(typeof(ViewMainDepartmentsInTheStore),with_exception_handling<ViewMainDepartmentsInTheStore>(() => new ViewMainDepartmentsInTheStore(catalog, renderer)));
+            all_factories.Add(factory_for<FrontController, DefaultFrontController>(() => front_controller));
+            all_factories.Add(factory_for<RequestFactory, RequestFactory>(() => new StubRequestFactory()));
+            all_factories.Add(factory_for<Renderer, RequestFactory>(() => renderer));
+            all_factories.Add(factory_for<ViewMainDepartmentsInTheStore, ViewMainDepartmentsInTheStore>(() => new ViewMainDepartmentsInTheStore(catalog, renderer)));
+
             return all_factories;
         }
+        static WebFormViewFactory get_webform_factory()
+        {
+            return new WebFormViewFactory(new StubFormPathRegistry(), BuildManager.CreateInstanceFromVirtualPath);
+        }
 
+        static KeyValuePair<Type,DependencyFactory> factory_for<SomeType,TypeToCreate>(Func<object> factory)
+        {
+            return new KeyValuePair<Type, DependencyFactory>(typeof(SomeType), with_exception_handling<TypeToCreate>(factory));
+        }
 
         static DependencyFactory with_exception_handling<TypeToCreate>(Func<object> factory)
         {
@@ -51,4 +51,5 @@ namespace nothinbutdotnetstore.tasks.startup
                 new BasicDependencyFactory(factory)); 
         }
     }
+
 }
