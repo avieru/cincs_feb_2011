73f650798f9b96a7e178271b8cc762c66bff1582
Andrei <avieru@gmail.com>
2011-02-11 11:26:21 -0500

    Pushing new changes

--- product/nothinbutdotnetstore/core/containers/BasicDependencyFactory.cs
+++ product/nothinbutdotnetstore/core/containers/BasicDependencyFactory.cs
@@ -2,11 +2,20 @@ using System;
 
 namespace nothinbutdotnetstore.core.containers
 {
+    public delegate object DependencyItem();
+
     public class BasicDependencyFactory : DependencyFactory
     {
+        readonly Func<object> dependency_item;
+
+        public BasicDependencyFactory(Func<object> dependency_item)
+        {
+            this.dependency_item = dependency_item;
+        }
+
         public object create()
         {
-            throw new NotImplementedException();
+            return dependency_item();
         }
     }
 }
