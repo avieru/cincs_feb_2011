de57d653abc519d311994aba44a9a776fb83b599
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2011-02-11 10:33:25 -0500

    Continuing containers

--- product/nothinbutdotnetstore.specs/BasicDependencyContainerSpecs.cs
+++ product/nothinbutdotnetstore.specs/BasicDependencyContainerSpecs.cs
@@ -1,3 +1,4 @@
+ using System;
  using System.Data;
  using System.Data.SqlClient;
  using Machine.Specifications;
@@ -20,14 +21,14 @@ namespace nothinbutdotnetstore.specs
         {
             Establish c = () =>
             {
-                the_connection = new SqlConnection();
                 dependency_factories = the_dependency<DependencyFactories>();
+                the_connection = new SqlConnection();
                 factory  = an<DependencyFactory>();
-                factory.Stub(x => x.create()).Return(the_connection);
-
 
                 dependency_factories.Stub(x => x.get_factory_that_can_create(typeof(IDbConnection)))
                     .Return(factory);
+
+                factory.Stub(x => x.create()).Return(the_connection);
             };
 
             Because b = () =>
@@ -41,6 +42,36 @@ namespace nothinbutdotnetstore.specs
             static IDbConnection the_connection;
             static DependencyFactories dependency_factories;
             static DependencyFactory factory;
+        }
+
+        public class when_the_factory_for_a_dependency_throws_an_exception_when_creating_its_dependency : concern
+        {
+            Establish c = () =>
+            {
+                dependency_factories = the_dependency<DependencyFactories>();
+                factory  = an<DependencyFactory>();
+                the_original_exception = new Exception();
+
+                dependency_factories.Stub(x => x.get_factory_that_can_create(typeof(IDbConnection)))
+                    .Return(factory);
+
+                factory.Stub(x => x.create()).Throw(the_original_exception);
+
+            };
+
+            Because b = () =>
+                catch_exception(() => sut.an<IDbConnection>());
+
+            It should_throw_a_dependency_creation_exception_with_the_correct_information = () =>
+            {
+                var item = exception_thrown_by_the_sut.ShouldBeAn<DependencyCreationException>();
+                item.InnerException.ShouldEqual(the_original_exception);
+                item.dependency_that_could_not_be_created.ShouldEqual(typeof(IDbConnection));
+            };
+
+            static DependencyFactories dependency_factories;
+            static DependencyFactory factory;
+            static Exception the_original_exception;
         }
     }
 }
--- product/nothinbutdotnetstore/core/containers/BasicDependencyContainer.cs
+++ product/nothinbutdotnetstore/core/containers/BasicDependencyContainer.cs
@@ -1,12 +1,23 @@
-using System;
-
 namespace nothinbutdotnetstore.core.containers
 {
     public class BasicDependencyContainer : DependencyContainer
     {
+        DependencyFactories dependency_factories;
+
+        public BasicDependencyContainer(DependencyFactories dependency_factories)
+        {
+            this.dependency_factories = dependency_factories;
+        }
+
         public Dependency an<Dependency>()
         {
-            throw new NotImplementedException();
+            return cast_to<Dependency>(
+                dependency_factories.get_factory_that_can_create(typeof(Dependency)).create());
+        }
+
+        Dependency cast_to<Dependency>(object dependency)
+        {
+            return (Dependency) dependency;
         }
     }
 }
