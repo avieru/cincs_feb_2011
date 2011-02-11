cae01ca4c8535311a8bb49716a12d6c3b7d7e8e0
Steve Brettschneider <steve.brettschneider@pinnsg.com>
2011-02-11 10:47:54 -0500

    Pushing new changes

--- product/nothinbutdotnetstore/core/containers/BasicDependencyContainer.cs
+++ product/nothinbutdotnetstore/core/containers/BasicDependencyContainer.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace nothinbutdotnetstore.core.containers
 {
     public class BasicDependencyContainer : DependencyContainer
@@ -11,8 +13,16 @@ namespace nothinbutdotnetstore.core.containers
 
         public Dependency an<Dependency>()
         {
-            return cast_to<Dependency>(
-                dependency_factories.get_factory_that_can_create(typeof(Dependency)).create());
+            try
+            {
+                return
+                    cast_to<Dependency>(
+                    dependency_factories.get_factory_that_can_create(typeof(Dependency)).create());
+            }
+            catch (Exception ex)
+            {
+                DependencyCreationException newExc = new DependencyCreationException(ex, typeof(Dependency));
+            }
         }
 
         Dependency cast_to<Dependency>(object dependency)
--- product/nothinbutdotnetstore/core/containers/DependencyCreationException.cs
+++ product/nothinbutdotnetstore/core/containers/DependencyCreationException.cs
@@ -4,6 +4,11 @@ namespace nothinbutdotnetstore.core.containers
 {
     public class DependencyCreationException : Exception
     {
+        public DependencyCreationException(Exception inner, Type the_type) : base("", inner)
+        {
+            dependency_that_could_not_be_created = the_type;
+        }
+
         public Type dependency_that_could_not_be_created { get; private set; }
     }
 }
