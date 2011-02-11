a58a5067e466788159e40d615769c3c07ae67101
Steve Brettschneider <steve.brettschneider@pinnsg.com>
2011-02-11 14:52:17 -0500

    Pushing new changes

--- product/nothinbutdotnetstore/core/containers/MagicWiringDependencyFactory.cs
+++ product/nothinbutdotnetstore/core/containers/MagicWiringDependencyFactory.cs
@@ -1,12 +1,36 @@
 using System;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace nothinbutdotnetstore.core.containers
 {
     public class MagicWiringDependencyFactory : DependencyFactory
     {
+        readonly Type type_being_created;
+        readonly DependencyContainer container;
+        readonly ConstructorSelectionStrategy strategy;
+
+        public MagicWiringDependencyFactory(Type type_being_created, DependencyContainer container, ConstructorSelectionStrategy strategy)
+        {
+            this.type_being_created = type_being_created;
+            this.container = container;
+            this.strategy = strategy;
+        }
+
         public object create()
         {
-            throw new NotImplementedException();
+            var constructor = strategy.get_applicable_constructor_on(type_being_created);
+            List<object> parameters = new List<object>();
+            foreach (var parameter in constructor.GetParameters())
+            {
+                parameters.Add(get_parameter_value(parameter));
+            }
+            return constructor.Invoke(parameters.ToArray());
+        }
+
+        private object get_parameter_value(ParameterInfo parameter)
+        {
+            return container.an(parameter.ParameterType);
         }
     }
 }
